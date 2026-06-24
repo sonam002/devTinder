3eda90d9c88f5a3281834b9311a11a9748e819a0
sonamverma28045@gmail.com <sonamverma28045@gmail.com>
2026-06-25 02:17:48 +0530

    Why do we even need middlewares? Solution

--- src/app.js
+++ src/app.js
@@ -2,17 +2,27 @@ const express = require("express")
 
 const app = express(); // instance of express
 
+//Handle Auth Middleware for GET, PUT, POST...requests
 //Why do we even need middlewares? 
 // Problem : We will have to write the same logic again and again for each route
-app.get("/admin/getAllData", (req, res) => {
+//Solution : We can make a common route /admin which can handle all routes starting with /admin
+app.use("/admin", (req, res, next) => {
     //Logic of fetching the data
-    const token = "abcd";
+    const token = "xyz";
     const isAdminAuthorized = token === "xyz";
-    if(isAdminAuthorized){
-        res.send("All Data Sent");
-    }else{
+    if(!isAdminAuthorized){
         res.status(401).send("Unauthorized request");
+    }else{
+        next();
     }
+});
+
+app.get("/user", (req, res, next) => {
+   res.send("User Data Sent");
+});
+
+app.get("/admin/getAllData", (req, res, next) => {
+   res.send("All Data Sent");
 });
 
 app.get("/admin/deleteUser", (req, res) => {
