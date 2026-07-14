3d94ec37257e8eaa05481c5df074bd956f825e67
sonamverma28045@gmail.com <sonamverma28045@gmail.com>
2026-07-15 01:25:28 +0530

    Schema Validations

--- src/models/user.js
+++ src/models/user.js
@@ -2,23 +2,41 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema({
     firstName: {
-        type: String
+        type: String,
+        required: true,
+        minLength: 4,
     },
     lastName: {
         type: String
     },
     emailId: {
-        type: String
+        type: String,
+        lowercase: true,
+        required: true,
+        unique: true,
+        trim: true,
     },
     password: {
-        type: String
+        type: String,
+        required: true,
     },
     age: {
-        type: Number
+        type: Number,
+        min: 18, // for number only min and for string minLength
     },
     gender: {
-        type: String
-    }
+        type: String,
+    },
+    photoUrl: {
+        type: String,
+    },
+    about: {
+        type: String,
+        default: "This is default description of the user!",
+    },
+    skills: {
+        type: [String],
+    },
 });
 
 const User = mongoose.model("User", userSchema);
