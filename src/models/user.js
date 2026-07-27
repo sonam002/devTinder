267bfb22fece2d695355c9a5989160626551f060
sonamverma28045@gmail.com <sonamverma28045@gmail.com>
2026-07-27 17:13:14 +0530

    Exploring Validator Library

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
     firstName: {
@@ -15,10 +16,20 @@ const userSchema = new mongoose.Schema({
         required: true,
         unique: true,
         trim: true,
+        validate(value){
+            if(!validator.isEmail(value)){
+                throw new Error("Invalid email address" + value);
+            }
+        }
     },
     password: {
         type: String,
         required: true,
+        validate(value) {
+            if (!validator.isStrongPassword(value)) {
+            throw new Error("Enter a Strong Password: " + value);
+            }
+        }
     },
     age: {
         type: Number,
