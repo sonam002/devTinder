135957979cba033e968129faa449f859f5ce4e68
sonamverma28045@gmail.com <sonamverma28045@gmail.com>
2026-07-27 16:58:45 +0530

    API level data sanitization

--- src/app.js
+++ src/app.js
@@ -63,12 +63,12 @@ app.delete("/user", async (req, res) => {
 })
 
 //Update data of the user - Any other data apart from schema will be ignored by mongo and not be updated
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId; //instead or req.body.userId we can put params and pass userId in URL cause its not good to update userId
   const data = req.body;
 
   try {
-    const ALLOWED_UPDATES = ["userId", "photoUrl", "about", "gender", "age", "skills"];
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
     const isUpdateAllowed = Object.keys(data).every((k) =>
       ALLOWED_UPDATES.includes(k)
     );
