1da8e9d12028b4a42e6d2ba03b1db3d6291eafac
sonamverma28045@gmail.com <sonamverma28045@gmail.com>
2026-08-03 00:33:56 +0530

    If this expiresIn is 0d then we will get jwt expired error

--- src/models/user.js
+++ src/models/user.js
@@ -63,7 +63,7 @@ const userSchema = new mongoose.Schema({
 userSchema.methods.getJWT = async function () { // no arrow fun as with this keyword we cannot use 
     const user = this;
     const token = await jwt.sign({ _id :  user._id }, "DEV@Tinder$790", {
-        expiresIn: "0d",
+        expiresIn: "7d",
     });
     return token;
 };
